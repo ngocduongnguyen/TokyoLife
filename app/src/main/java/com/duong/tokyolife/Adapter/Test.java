33aaf86d49c64378aec3f8dff213083584424636
ngocduongnguyen <ngocduongnguyen1998@gmail.com>
2019-03-16 19:21:27 +0700

    Commit

---
+++ app/src/main/java/com/duong/tokyolife/Adapter/Test.java
@@ -0,0 +1,21 @@
+package com.duong.tokyolife.Adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class Test extends FragmentPagerAdapter {
+    public Test(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
