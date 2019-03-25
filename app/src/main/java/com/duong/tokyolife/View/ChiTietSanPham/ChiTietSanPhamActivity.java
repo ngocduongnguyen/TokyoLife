c2a5127c5eb9d6c93026387cba1d7079983c3e11
ngocduongnguyen <ngocduongnguyen1998@gmail.com>
2019-03-25 14:25:03 +0700

    Update

---
+++ app/src/main/java/com/duong/tokyolife/View/ChiTietSanPham/ChiTietSanPhamActivity.java
@@ -0,0 +1,15 @@
+package com.duong.tokyolife.View.ChiTietSanPham;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.duong.tokyolife.R;
+
+public class ChiTietSanPhamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_tiet_san_pham);
+    }
+}
